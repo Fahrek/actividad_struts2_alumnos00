f0a39fd7273d3a8a61dc20d070c01327d5a8ac76
Andres G. Quina <andresgqjob@gmail.com>
2024-02-06 12:27:52 +0100

    Initial commit

---
+++ src/main/java/org/demo/actions/beans/InvoiceBean.java
@@ -0,0 +1,45 @@
+package org.demo.actions.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class InvoiceBean implements Serializable {
+
+    private String subject;
+    private Date dateFrom;
+    private Date dateTo;
+    private double grossAmount;
+
+
+    public double getGrossAmount() {
+        return grossAmount;
+    }
+
+    public void setGrossAmount(double grossAmount) {
+        this.grossAmount = grossAmount;
+    }
+
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public Date getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
